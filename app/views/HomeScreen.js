6acd9bb4f7637aedf8ed603df8b7db362bb41364
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-14 00:10:01 +0530

    added dynamic list using RN-FlatList

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -1,16 +1,31 @@
 import React, {Fragment} from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, FlatList } from 'react-native';
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
       title: 'Welcome',
     };
+
+    constructor() {
+        super();
+        // const ds = new FlatList.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});  
+        this.state = {
+            dataSource: ['Android','iOS', 'Java','Php', 'Hadoop',  
+            'Sap', 'Python','Ajax', 'C++',  
+            'Ruby', 'Rails','.Net', 'Perl'],  
+        }
+    }
     render() {
       const {navigate} = this.props.navigation;
       return (
-        <Button
-          title="Go to Jane's profile"
-          onPress={() => navigate('Profile', {name: 'Jane'})}
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => {
+            return (
+                <Text style={{fontSize:22}}>{item}</Text>
+            )
+        }}
+              
         />
       );
     }
