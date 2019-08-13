bbb0b94d391194b2679bdef827276b63fda774d5
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-14 00:35:26 +0530

    fetched data from api end point using RN-fetch API

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -10,11 +10,25 @@ class HomeScreen extends React.Component {
         super();
         // const ds = new FlatList.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});  
         this.state = {
-            dataSource: ['Android','iOS', 'Java','Php', 'Hadoop',  
-            'Sap', 'Python','Ajax', 'C++',  
-            'Ruby', 'Rails','.Net', 'Perl'],  
+            dataSource:'',
         }
     }
+
+    async componentDidMount() {
+        //Have a try and catch block for catching errors.
+        try {
+            //Assign the promise unresolved first then get the data using the json method. 
+            const pokemonApiCall = await fetch('https://facebook.github.io/react-native/movies.json');
+            const pokemon = await pokemonApiCall.json();
+            this.setState({dataSource: JSON.stringify(pokemon.movies), loading: false});
+            alert(this.state.dataSource);
+            console.log("fetching data-----------", pokemon.movies);
+        } catch(err) {
+            console.log("Error fetching data-----------", err);
+        }
+    }
+
+
     render() {
       const {navigate} = this.props.navigation;
       return (
@@ -22,7 +36,7 @@ class HomeScreen extends React.Component {
           data={this.state.dataSource}
           renderItem={({ item }) => {
             return (
-                <Text style={{fontSize:22}}>{item}</Text>
+                <Text style={{fontSize:22}}>{item.title}</Text>
             )
         }}
               
