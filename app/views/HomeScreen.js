f07a6b005fdc1a991fb6f1aef39f87050eacf07f
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-14 16:50:33 +0530

    Aadded RN-RefreshControl(pull to refresh)

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import { Button, View, Text, FlatList, ActivityIndicator, TouchableOpacity} from 'react-native';
+import { Button, View, Text, FlatList, ActivityIndicator, TouchableOpacity, ScrollView, RefreshControl} from 'react-native';
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
@@ -42,15 +42,20 @@ class HomeScreen extends React.Component {
                 dataSource: responseJson,
                 loading: false
             })
-            // return responseJson.posts;
         } catch (error) {
             console.error(error);
         }
     }
-    onPostPress () {
+    _onPostPress () {
         this.props.navigation.navigate('Profile', {title: "item.title"});
     }
 
+    _onRefresh = () => {
+        this.setState({refreshing: true});
+        this.getPostsFromApi().then(() => {
+          this.setState({refreshing: false});
+        });
+    }
 
     render() {
         const { loading } = this.state; 
@@ -58,28 +63,39 @@ class HomeScreen extends React.Component {
 
           if (!loading) {
               return (
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={({ item }) => {
-                        return (
-                            <TouchableOpacity
-                                // {...data.item}
-                                // onPress={()=>this.onPostPress(item.title)}
-                                onPress={() => {
-                                    /* 1. Navigate to the Details route with params */
-                                    this.props.navigation.navigate('Profile', {
-                                      id: item.id,
-                                      title: item.title,
-                                      body: item.body
-                                    });
-                                  }}
-                            >
-                                <Text style={{fontSize:22}}>{item.title}</Text>
-                            </TouchableOpacity>
-                            
-                        )
-                    }}      
-                />
+                <ScrollView
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh}
+                        />
+                    }>   
+                        <FlatList
+                            data={this.state.dataSource}
+                            renderItem={({ item }) => {
+                                return (
+                                    <TouchableOpacity
+                                        // {...data.item}
+                                        // onPress={()=>this._onPostPress(item.title)}
+                                        onPress={() => {
+                                            /* 1. Navigate to the Details route with params */
+                                            this.props.navigation.navigate('Profile', {
+                                            id: item.id,
+                                            title: item.title,
+                                            body: item.body
+                                            });
+                                        }}
+                                    >
+                                        <View style={{ backgroundColor: '#E4E4E4', alignItems:'center', justifyContent:'center', borderRadius: 20, padding: 5,  margin:10, minHeight: 40}}>
+                                            <Text style={{fontSize:22}}>{item.title}</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                )
+                            }}      
+                        />
+              </ScrollView>
+                
               )
           } else {
                 return (
