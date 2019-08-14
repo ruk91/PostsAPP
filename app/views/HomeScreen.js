4d6d181909774d6b3512f6d7e571e836c941c761
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-14 12:41:17 +0530

    data fetching and displaying using FlatList

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -1,47 +1,76 @@
 import React, {Fragment} from 'react';
-import { Button, View, Text, FlatList } from 'react-native';
+import { Button, View, Text, FlatList, ActivityIndicator } from 'react-native';
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
-      title: 'Welcome',
+      title: 'Posts List',
     };
 
     constructor() {
         super();
         // const ds = new FlatList.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});  
         this.state = {
+            loading: true,
             dataSource:'',
         }
     }
 
-    async componentDidMount() {
-        //Have a try and catch block for catching errors.
+    componentDidMount() {
+        this.getPostsFromApi();
+        // try {
+        //     let response = await fetch(
+        //       'https://jsonplaceholder.typicode.com/posts',
+        //     );
+        //     let responseJson = await response.json();
+        //     this.setState({
+        //         dataSource: responseJson,
+        //         loading: false
+        //     })
+        //     // return responseJson.movies;
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    }
+
+    async getPostsFromApi() {
         try {
-            //Assign the promise unresolved first then get the data using the json method. 
-            const pokemonApiCall = await fetch('https://facebook.github.io/react-native/movies.json');
-            const pokemon = await pokemonApiCall.json();
-            this.setState({dataSource: JSON.stringify(pokemon.movies), loading: false});
-            alert(this.state.dataSource);
-            console.log("fetching data-----------", pokemon.movies);
-        } catch(err) {
-            console.log("Error fetching data-----------", err);
+            let response = await fetch(
+              'https://jsonplaceholder.typicode.com/posts',
+            );
+            let responseJson = await response.json();
+            this.setState({
+                dataSource: responseJson,
+                loading: false
+            })
+            // return responseJson.movies;
+        } catch (error) {
+            console.error(error);
         }
     }
 
 
     render() {
-      const {navigate} = this.props.navigation;
-      return (
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({ item }) => {
-            return (
-                <Text style={{fontSize:22}}>{item.title}</Text>
-            )
-        }}
-              
-        />
-      );
+        console.log('Inside render');
+        const { loading } = this.state; 
+        const { navigate } = this.props.navigation;
+
+          if (!loading) {
+              return (
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({ item }) => {
+                        return (
+                            <Text style={{fontSize:22}}>{item.title}</Text>
+                        )
+                    }}      
+                />
+              )
+                
+          } else {
+                return (
+                    <ActivityIndicator/>
+                )
+          }   
     }
 }
 export default HomeScreen;
